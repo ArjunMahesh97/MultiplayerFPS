21e782ad7b9f703fee86aad072aadbac1bc8ba2c
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-19 18:52:18 +0530

    Camera rotation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -24,13 +24,14 @@ public class PlayerController : MonoBehaviour {
         Vector3 movVertical = transform.forward * zMov;
 
         Vector3 vel = (movHorizontal + movVertical).normalized * speed;
-
         motor.Move(vel);
 
         float yRot = Input.GetAxisRaw("Mouse X");
-
         Vector3 rot = new Vector3(0f, yRot, 0f) * lookSensitivity;
-
         motor.Rotate(rot);
-	}
+
+        float xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 cam_rot = new Vector3(xRot, 0f, 0f) * lookSensitivity;
+        motor.RotateCamera(cam_rot);
+    }
 }
--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -8,6 +8,10 @@ public class PlayerMotor : MonoBehaviour {
 
     private Vector3 velocity = Vector3.zero;
     private Vector3 rotation = Vector3.zero;
+    private Vector3 camrotation = Vector3.zero;
+
+    public Camera cam;
+
 
     private Rigidbody rigidBody;
 
@@ -35,6 +39,11 @@ public class PlayerMotor : MonoBehaviour {
     private void PerformRotation()
     {
         rigidBody.MoveRotation(transform.rotation * Quaternion.Euler(rotation));
+
+        if (cam != null)
+        {
+            cam.transform.Rotate(-camrotation);
+        }
     }
 
     private void PerformMovement()
@@ -48,5 +57,10 @@ public class PlayerMotor : MonoBehaviour {
     public void Rotate(Vector3 rot)
     {
         rotation = rot;
+    }
+
+    public void RotateCamera(Vector3 cam_rot)
+    {
+        camrotation = cam_rot;
     }
 }
