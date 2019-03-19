7383abe163586aa0c1faba93e76751f2fab8ba5f
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-19 18:37:04 +0530

    Player rotation

---
+++ Assets/PlayerMotor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Vector3 vel)
+    {
+        velocity = vel;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformRotation()
+    {
+        rigidBody.MoveRotation(transform.rotation * Quaternion.Euler(rotation));
+    }
+
+    private void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rigidBody.MovePosition(transform.position + velocity*Time.fixedDeltaTime);
+        }
+    }
+
+    public void Rotate(Vector3 rot)
+    {
+        rotation = rot;
+    }
+}
