e955de5bd7c298f0c67d19320577c04473b28e34
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-20 02:21:45 +0530

    Sync Movements

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 rotation = Vector3.zero;
+    private Vector3 camrotation = Vector3.zero;
+
+    public Camera cam;
+
+
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Vector3 vel)
+    {
+        velocity = vel;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformRotation()
+    {
+        rigidBody.MoveRotation(transform.rotation * Quaternion.Euler(rotation));
+
+        if (cam != null)
+        {
+            cam.transform.Rotate(-camrotation);
+        }
+    }
+
+    private void PerformMovement()
+    {
+        if (velocity != Vector3.zero)
+        {
+            rigidBody.MovePosition(transform.position + velocity*Time.fixedDeltaTime);
+        }
+    }
+
+    public void Rotate(Vector3 rot)
+    {
+        rotation = rot;
+    }
+
+    public void RotateCamera(Vector3 cam_rot)
+    {
+        camrotation = cam_rot;
+    }
+}
---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    public Behaviour[] componentsToDisable;
+
+    Camera sceneCamera;
+
+	// Use this for initialization
+	void Start () {
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+	}
+
+    private void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
