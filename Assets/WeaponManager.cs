ec08624df0d3b2c9847be2ea74f5a90ac7c8bee5
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-03-25 00:56:21 +0530

    WeaponManager

---
+++ Assets/WeaponManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WeaponManager : NetworkBehaviour {
+
+    [SerializeField] string weaponLayerName = "Weapon";
+    [SerializeField] private PlayerWeapon primaryWeapon;
+
+    private PlayerWeapon currentWeapon;
+
+	// Use this for initialization
+	void Start () {
+        EquipWeapon(primaryWeapon);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void EquipWeapon(PlayerWeapon weapon){
+        currentWeapon = weapon;
+    }
+
+    public PlayerWeapon GetCurrentWeapon()
+    {
+        return currentWeapon;
+    }
+}
