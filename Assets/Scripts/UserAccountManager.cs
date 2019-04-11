352b52c00f379c50d51c53606dcff6ef7906682e
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-04-12 01:27:28 +0530

    Login menu

---
+++ Assets/Scripts/UserAccountManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserAccountManager : MonoBehaviour {
+
+    public static UserAccountManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
